42b4ffd26f743116b9e177fdef1a0af4795e3780
Ilker Tacyildiz <tacyildizilker@gmail.com>
2022-08-29 23:00:46 +0300

    Firebase - FeedVC

---
+++ BasicSnapchatClone/Model/Snap.swift
@@ -0,0 +1,16 @@
+//
+//  Snap.swift
+//  BasicSnapchatClone
+//
+//  Created by ILKER on 29.08.2022.
+//
+
+import Foundation
+
+struct Snap {
+    
+    var username: String
+    var imageUrlArray: [String]
+    var date: Date
+    
+}
--- BasicSnapchatClone/ViewControllers/SnapVC.swift
+++ BasicSnapchatClone/ViewControllers/SnapVC.swift
@@ -9,10 +9,15 @@ import UIKit
 
 class SnapVC: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var selectedSnap : Snap?
+    var selectedTime : Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        timeLabel.text = "Time Left: \(selectedTime!)"
     }
     
 
